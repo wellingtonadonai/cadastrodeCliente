cee58f8bbb2bd1f2c92be3377f73260b47a49eb0
wellington <wellington.h.s.oliveira@hotmail.com>
2025-02-25 22:10:44 -0300

    cadastro de cliente

---
+++ src/Wellington/henrique/DAO/IClienteDAO.java
@@ -0,0 +1,20 @@
+package Wellington.henrique.DAO;
+
+import Wellington.henrique.domain.Cliente;
+
+import java.util.Collection;
+import java.util.Collection;
+import java.util.List;
+
+public interface IClienteDAO {
+
+    public boolean cadastrar(Cliente cliente);
+
+    public void excluir(Long cpf);
+
+    public void alterar(Cliente cliente);
+
+    public Cliente consultar(Long cpf);
+
+    public Collection<Cliente> buscarTodos();
+}
